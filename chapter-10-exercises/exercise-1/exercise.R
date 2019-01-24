6c04d95120fd634ae9a488e64e39a5bb9a5a7a91
mayasioson <mayas1@uw.edu>
2019-01-23 21:45:46 -0800

    Complete chapter 10 exercises 1-4

--- chapter-10-exercises/exercise-1/exercise.R
+++ chapter-10-exercises/exercise-1/exercise.R
@@ -2,28 +2,28 @@
 
 # Create a vector of the number of points the Seahawks scored in the first 4 games
 # of the season (google "Seahawks" for the scores!)
-
+seahawks_points <- c(24, 17, 24, 20)
 
 # Create a vector of the number of points the Seahwaks have allowed to be scored
 # against them in each of the first 4 games of the season
-
+opponent_points <- c(27, 24, 13, 17)
 
 # Combine your two vectors into a dataframe called `games`
-
+games <- data.frame(seahawks_points, opponent_points)
 
 # Create a new column "diff" that is the difference in points between the teams
 # Hint: recall the syntax for assigning new elements (which in this case will be
 # a vector) to a list!
-
+games[["diff"]] <- abs(seahawks_points - opponent_points)
 
 # Create a new column "won" which is TRUE if the Seahawks won the game
-
+games[["won"]] <- seahawks_points > opponent_points
 
 # Create a vector of the opponent names corresponding to the games played
-
+opponent_names <- c("Broncos", "Bears", "Cowboys", "Cardinals")
 
 # Assign your dataframe rownames of their opponents
-
+rownames(games) <- opponent_names
 
 # View your data frame to see how it has changed!
-
+View(games)
--- chapter-10-exercises/exercise-4/exercise.R
+++ chapter-10-exercises/exercise-4/exercise.R
@@ -4,36 +4,36 @@
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
+gates_grants <- read.csv("data/gates_money.csv", stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
-
+View(gates_grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-
+organization <- gates_grants$organization
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
-
+is.vector(organization)
 
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-
+mean(gates_grants$total_amount)
 
 # What was the dollar amount of the largest grant?
-
+largest <- max(gates_grants$total_amount)
 
 # What was the dollar amount of the smallest grant?
-
+smallest <- min(gates_grants$total_amount)
 
 # Which organization received the largest grant?
-
+gates_grants[gates_grants$total_amount == largest, "organization"]
 
 # Which organization received the smallest grant?
-
+gates_grants[gates_grants$total_amount == smallest, "organization"]
 
 # How many grants were awarded in 2010?
-
+nrow(gates_grants[gates_grants$start_year == 2010, ])
